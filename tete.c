b2e498311b44d7a0aa85b614234aae40e8429018
王俊宇 <12531206+MMBBi@user.noreply.gitee.com>
2023-03-03 15:11:08 +0000

    3.3日刷题代码
    Signed-off-by: 王俊宇 <12531206+MMBBi@user.noreply.gitee.com>

---
+++ tete.c
@@ -0,0 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//    int x = 100;
+//    printf("x=%d\n", x++);//100,先取值100，自+1后101计算下面一行
+//    printf("x=%d\n", ++x);//102，上面一行运算为101，先运算，再取值，101+1=102
+//    printf("x=%d\n", --x);//101，与上面同理.101
+//    printf("x=%d\n", x--);//101，于第6行同理，先取值了。
+//    return 0;
+//}
+//int main()
+//{
+//    //定义小编兜里的钱
+//    double money = 12;
+//    //定义打车回家的费用
+//    double cost = 11.5;
+//    printf("小编能不能打车回家呢：");
+//    //输出y小编就打车回家了，输出n小编就不能打车回家
+//    printf("%c\n", money >= cost ? 'y' : 'n');
+//    return 0;
+//}
+//int main()
+//{
+//    int year = 2014; //今年是2014年
+//    //补全一下代码
+//    if ((year % 4 == 0 && year % 4 !=100)||(year%400==0))
+//    {
+//        printf("%s\n", "今年不是平年");
+//    }
+//    else
+//    {
+//        printf("%s\n", "今年是平年");
+//    }
+//
+//    return 0;
+//}
+//int main()
+//{
+//    int score = 7200;
+//    //完善一下代码
+//    if (score >= 10000)
+//    {
+//        printf("钻石玩家");
+//    }
+//    else if (score >= 5000 && score < 10000)
+//    {
+//        printf("白金玩家");
+//    }
+//    else if (score >= 1000 && score < 5000)
+//    {
+//        printf("青铜玩家");
+//    }
+//    else
+//    {
+//        printf("普通玩家");
+//    }
+//    
+//    return 0;
+//}
+//int main()
+//{
+//    int sale = 120; //销售业绩为120万
+//    int year = 1; //刚刚进入公司1年
+//    //完善代码
+//    if (sale > 100)
+//    {
+//        if (year >= 2)
+//            printf("获奖");
+//        else
+//            printf("很遗憾，期望你再接再厉");
+//    }
+//    else
+//    {
+//        printf("很遗憾，期望你再接再厉");
+//    }
+//    return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//    int number = 200;
+//    int year = 2014;
+//    do {
+//        year++;
+//        number *=1.2;                   
+//    } while (number < 1000);     
+//    printf("到%d年招工规模突破1000人\n", year);
+//    return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    printf("     **\n");
+//    printf("     **\n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *\n");
+//    printf("    *  *\n");
+//    return 0;
+//}
+//
+#include <stdio.h>
+
+//int main()
+//{
+//    int M = 0;
+//    scanf("%d\n", &M);
+//    if (M % 5 == 0)
+//    {
+//        printf("YES");
+//    }
+//    else
+//    {
+//        printf("NO");
+//    }
+//    return 0;
+//}
